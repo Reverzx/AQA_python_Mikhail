7b581f9be6f03b4648dff42f7f78a79bc11a01d3
Mikhail <Rixed@yandex.ru>
2025-02-08 06:08:21 +0700

    HW #5 corrected

---
+++ HW#5.py
@@ -0,0 +1,21 @@
+# Методы строк
+web_site = 'www.my_site.com#about'
+print(web_site.replace('#', '/'))
+print(web_site[:] + 'ing')
+print(" ".join('Ivanou Ivan'.split()[::-1]))
+print("   sad    ".strip())
+print("pARiS".capitalize())
+
+# Методы спиков
+print("Robin Singh".split())
+print("I love arrays they are my favorite".split())
+spisok = ['Ivan', 'Ivanou']
+stroka = 'Minsk Belarus'
+arr = stroka.split()
+print(f"Привет, {spisok[0]} {spisok[1]}! Добро пожаловать в "
+      f"{arr[0]} {arr[1]}")
+print(" ".join(["I", "love", "arrays", "they", "are", "my", "favorite"]))
+spisok_2 = ['Privet', 22, 123, 123, 'sdfsdf', 445, 546456, 'Foo', 'Baz', 453]
+spisok_2.insert(2, "center")
+del spisok_2[6]
+print(spisok_2)
