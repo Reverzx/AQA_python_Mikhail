6481fdf98913b8b33052d9ae3b42e93d49007bb6
Mikhail <Rixed@yandex.ru>
2025-02-15 00:21:54 +0700

    HW#7

--- HW#7/Statues.py
+++ HW#7/Statues.py
@@ -1,5 +1,5 @@
 # x = [6, 1, 3, 8, 15]
-x = list(map(int,input("Введите размеры статуй через пробел: ").split()))
+x = list(map(int, input("Введите размеры статуй через пробел: ").split()))
 minim = min(x)
 maks = max(x)
-print(f"Не хватает {maks - minim - len(set(x)) +1} статуи")
+print(f"Не хватает {maks - minim - len(set(x)) + 1} статуи")
