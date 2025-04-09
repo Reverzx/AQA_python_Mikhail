0ea799505499726b65119798d55097b71dd5357a
Mikhail <Rixed@yandex.ru>
2025-04-10 03:29:52 +0700

    HW21_dbg

--- HW21/tests/conftest.py
+++ HW21/tests/conftest.py
@@ -17,8 +17,9 @@ def setup_logging(request):
     logger.remove()
     logger.add(
         sys.stderr,
-        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{"
-               "name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: "
+               "<8}</level> | <cyan>{name}</cyan>:<cyan>{function}"
+               "</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
         level=log_level,
         colorize=True
     )
