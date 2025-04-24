3a4c0665012f60e3c77b5a7994237695cb32ec51
Mikhail <Rixed@yandex.ru>
2025-04-24 09:33:56 +0700

    2

--- selenium_page_object/test_data/creds.py
+++
@@ -1,4 +0,0 @@
-class UserCredentials:
-  standart_user = "standard_user"
-  standart_password = "secret_sauce"
-  locked_user = "locked_out_user"
--- selenium_page_object/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture
-def driver():
-    browser = webdriver.Chrome()
-    yield browser
-    browser.quit()
